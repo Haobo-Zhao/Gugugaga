d057f2a2d6b6e842c99e17439df75f003c77d6d7
Haobo-Zhao <haobo4608@gmail.com>
2020-05-03 01:57:28 -0400

    implement unix-tree style presentation for expression

--- utils.js
+++ utils.js
@@ -1,12 +1,28 @@
 const log = console.log.bind(console)
 
-function prettyLog(node, indent='') {
-    const message = `${indent}${node.kind}${node && node.value ? ` ${node.value}` : ''}`
+const prettyLog = (node, indent = '', marker = '', isLastChild = true, isRoot = true) => {
+    let message = `${indent}${marker}${node.kind}`
+    if (node && node.value) {
+        message += `${node.value}`
+    }
     log(message)
 
-    indent += '    '
-    node.getChildren().forEach(child => {
-        prettyLog(child, indent)
+    const children = node.getChildren()
+    const lastChild = children[children.length - 1]
+
+    const parentIsLastChild = isLastChild
+    const parentIsRoot = isRoot
+    const parentIndent = indent
+    const childIndent = parentIndent + (parentIsRoot
+        ? ''
+        : `${parentIsLastChild ? '    ' : '│   '}`
+    )
+    children.forEach(child => {
+        const childMarker = child == lastChild
+            ? '└── '
+            : '├── '
+
+        prettyLog(child, childIndent, childMarker, child == lastChild, false)
     });
 }
 
